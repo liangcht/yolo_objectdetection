9105cb65ddf04cdbd4570ad2f6a1f2afb9e2813c
Apar Singhal <NORTHAMERICA.t-apsi@VIGDGX01.redmond.corp.microsoft.com>
2019-05-13 13:47:02 -0700

    Added file to test the working of mtorch/yolo_detector

---
+++ verify/verify_yolo_detector.py
@@ -0,0 +1,32 @@
+import os
+import cv2
+import time
+import numpy as np
+#os.environ['CUDA_VISIBLE_DEVICES'] = "4"
+
+from mtorch.yolo_detctor import YoloDetector
+
+path_model = '/home/t-apsi/github/objectdetection/test/files/model_epoch_36.pt'
+path_labelmap = '/home/t-apsi/github/objectdetection/test/files/labelmap0.txt'
+
+path_images  = '/home/t-apsi/github/objectdetection/test/images'
+
+detector = YoloDetector(path_model, path_labelmap, thresh=0, obj_thresh=0)
+for f, filename in enumerate(os.listdir(path_images)):
+    print(filename)
+    image = cv2.imread(os.path.join(path_images, filename))
+    res = detector.detect(image)
+    print(res)
+
+    # Save the images with bounding boxes
+    # for i, bbox in enumerate(res):
+    #     bbox['rect'] = [int(x) for x in bbox['rect']]
+    #     x1, y1, x2, y2 = bbox['rect']
+    #     temp = image.copy()
+    #     temp[y1-1:y1+1, x1:x2, :] = 0
+    #     temp[y2-1:y2+1, x1:x2, :] = 0
+    #     temp[y1:y2, x1-1:x1+1, :] = 0
+    #     temp[y1:y2, x2-1:x2+1, :] = 0
+
+    #     output_name = filename + '_' + bbox['class'] + '_' + str(int(bbox['conf']*100))
+    #     cv2.imwrite(os.path.join('test', 'saved', output_name +'.jpg'), temp)
