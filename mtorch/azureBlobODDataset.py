0dd54d1a006de38abd9ab5e50a89df7ac6c5f6fc
Liang-Chun Tsai <lits@microsoft.com>
2019-06-27 17:22:04 -0700

    Use numpy array

--- mtorch/azureBlobODDataset.py
+++ mtorch/azureBlobODDataset.py
@@ -42,13 +42,15 @@ class AzureBlobODDataset(torch.utils.data.Dataset):
             return image, index, h, w, targets
         else:
             # Convert absolute coordinates to (x1, y1, x2, y2)
-            ltwh_target = np.zeros(shape=(len(targets), 5), dtype=float)
+            ltwh_target = [None] * len(targets)
             for i, t in enumerate(targets):
                 bbox = t["BoundingBox"]
 
-                ltwh_target[i] = np.asarray([t['tagIndex'], bbox[0], bbox[1], bbox[2], bbox[3]])
+                ltwh_target[i] = [t['tagIndex'], bbox[0], bbox[1], bbox[2], bbox[3] ]
             #targets = np.array(abs_target)
             image, targets = self.transform(image, ltwh_target)
+            targets = np.asarray(targets)
+
             sample = {IMAGE: image, LABEL:targets}
             
             return sample[IMAGE], sample[LABEL]
