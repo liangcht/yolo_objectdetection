450cf0e0eaa100f5a8f5e32c0f88cca7ec3afce2
Liang-Chun Tsai <lits@microsoft.com>
2019-07-02 13:53:28 -0700

    Use absolute value

--- mtorch/azureBlobODDataset.py
+++ mtorch/azureBlobODDataset.py
@@ -48,10 +48,11 @@ class AzureBlobODDataset(torch.utils.data.Dataset):
             return sample, index, h, w, iris_target
         else:
             # Convert absolute coordinates to (x1, y1, x2, y2)
+            w, h = image.size
             abs_target = [None] * len(target)
             for i, t in enumerate(target):
                 bbox = t["BoundingBox"]
-                abs_target[i] = [bbox[0], bbox[1], bbox[0] + bbox[2], bbox[1] + bbox[3], t['tagIndex']]
+                abs_target[i] = [bbox[0] * w, bbox[1] * h, (bbox[0] + bbox[2]) * w, (bbox[1] + bbox[3]) * h, t['tagIndex']]
             target = np.array(abs_target)
             sample = {IMAGE: image, LABEL:target}
             sample = self.transform(sample)
