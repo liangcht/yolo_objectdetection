6224886893621572d4ca66c94ab58f5663342c14
Liang-Chun Tsai <lits@microsoft.com>
2019-07-03 09:45:36 -0700

    State_dict

--- mmod/yolo_iris_train_IRISTorch.py
+++ mmod/yolo_iris_train_IRISTorch.py
@@ -191,7 +191,7 @@ def main(args, log_pth):
     model = Yolo(num_classes = len(cmap))
 
     model_dict = torch.load("_epoch_1.pt")
-    model.load_state_dict(model_dict, strict=True)
+    model.load_state_dict(model_dict["state_dict"], strict=True)
     model.to_device()
 
     with open(trainingManifestFile) as json_data:
