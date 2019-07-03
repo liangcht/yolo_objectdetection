38c5d335dd6ee72c3940f38f6f63fd1ecaa483bc
Liang-Chun Tsai <lits@microsoft.com>
2019-07-03 13:32:47 -0700

    Threshold 0

--- mmod/yolo_iris_train_IRISTorch.py
+++ mmod/yolo_iris_train_IRISTorch.py
@@ -85,7 +85,6 @@ def eval(model, num_classes, test_loader):
                 if pre_box[0] == 0:
                     del result[pre_idx]
             results.append(result)
-            print(result)
 
         # measure elapsed time
         batch_time.update(time.time() - end)
