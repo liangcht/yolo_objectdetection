9c9080a2a2cb7d46eec4c2856de459d360b2c12f
Liang-Chun Tsai <lits@microsoft.com>
2019-07-05 09:47:43 -0700

    Use .

--- mmod/yolo_iris_train_IRISTorch.py
+++ mmod/yolo_iris_train_IRISTorch.py
@@ -206,8 +206,8 @@ def main(args, log_pth):
     cmap = load_labelmap_list(label_map)
     model = Yolo(num_classes = len(cmap))
 
-    if args["eval_only"]:
-        model_dict = torch.load(args["model_file"])
+    if args.eval_only:
+        model_dict = torch.load(args.model_file)
         model.load_state_dict(model_dict["state_dict"], strict=True)
 
         # with open(trainingManifestFile) as json_data:
