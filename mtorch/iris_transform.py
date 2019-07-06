18d13125029b7eac1b0e219a0f19a254b1498ff1
Liang-Chun Tsai <lits@microsoft.com>
2019-07-05 17:21:30 -0700

    Debug

--- mtorch/iris_transform.py
+++ mtorch/iris_transform.py
@@ -192,7 +192,7 @@ class CenterCropTransform(Transform):
 
 class IrisODTransform(Transform):
     def __init__(self, input_size):
-        self.transforms = [ODImResize(),#ODImageTransform(torchvision.transforms.Resize(input_size)),
+        self.transforms = [ODImageTransform(torchvision.transforms.Resize(input_size)),
                            #ODCenterCrop(input_size),
                            ODImageTransform(torchvision.transforms.ToTensor()),
                            ODImageTransform(torchvision.transforms.Normalize([0.482, 0.459, 0.408], [1, 1, 1]))]
