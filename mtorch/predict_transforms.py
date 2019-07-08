eae671ae60e46306c68fe792d7fb3bd828aac23f
Liang-Chun Tsai <lits@microsoft.com>
2019-07-08 14:09:06 -0700

    Debug

--- mtorch/predict_transforms.py
+++ mtorch/predict_transforms.py
@@ -22,9 +22,7 @@ class ODImResize(object):
         im_resized = im_rescale(im.astype(np.float32, copy=True), 
                                 max(self.network_input_width, self.network_input_height))
         new_h, new_w = im_resized.shape[0:2]
-        print(im.shape[0])
-        print(new_h)
-        print(new_w)
+        print(im_resized.shape[0:2])
         left = int(np.round((self.network_input_width - new_w) / 2))
         right = int(np.round(self.network_input_width - new_w - left))
         top = int(np.round((self.network_input_height - new_h) / 2))
