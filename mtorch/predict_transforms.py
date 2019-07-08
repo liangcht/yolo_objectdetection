c95cee59fab35aedd3c2a8e30c720b4afdf22a65
Liang-Chun Tsai <lits@microsoft.com>
2019-07-08 13:58:59 -0700

    Update formula

--- mtorch/predict_transforms.py
+++ mtorch/predict_transforms.py
@@ -52,8 +52,10 @@ class ODImResize(object):
 
     def _set_network_input_size(self, h, w):
         if h > w:
-            self.network_input_height = h * 416.0 / w
+            self.network_input_height = ((416 * w + h - 1) // h +
+                                            31) // 32 * 32
             self.network_input_width = 416
         else:
-            self.network_input_width =  w * 416.0 / h
+            self.network_input_width =  ((416 * w + h - 1) // h +
+                                            31) // 32 * 32
             self.network_input_height = 416
