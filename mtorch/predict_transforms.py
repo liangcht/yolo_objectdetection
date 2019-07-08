0796107c947b46bed16d3daf8f89fcb378b8ac4a
Liang-Chun Tsai <lits@microsoft.com>
2019-07-08 14:14:00 -0700

    Debug

--- mtorch/predict_transforms.py
+++ mtorch/predict_transforms.py
@@ -22,16 +22,15 @@ class ODImResize(object):
         im_resized = im_rescale(im.astype(np.float32, copy=True), 
                                 max(self.network_input_width, self.network_input_height))
         new_h, new_w = im_resized.shape[0:2]
-        print(im_resized.shape[0:2])
         left = int(np.round((self.network_input_width - new_w) / 2))
         right = int(np.round(self.network_input_width - new_w - left))
         top = int(np.round((self.network_input_height - new_h) / 2))
         bottom = int(np.round((self.network_input_height - new_h - top)))
         im_squared = cv2.copyMakeBorder(im_resized, top=top, bottom=bottom, left=left, right=right,
                                     borderType=cv2.BORDER_CONSTANT, value=[0, 0, 0])
- 
+        print(im_squared.shape[0:2])
         return im_squared
-    '''
+
     def _set_network_input_size(self, h, w):
         if self.maintain_ratio:
             alpha = np.sqrt(self.target_size[0] * self.target_size[1]) / np.sqrt(h * w)
@@ -48,8 +47,8 @@ class ODImResize(object):
         else:
             self.network_input_height = self.target_size[0]
             self.network_input_width = self.target_size[1]
-    '''
 
+    '''
     def _set_network_input_size(self, h, w):
         if h > w:
             self.network_input_height = ((416.0 * h / w) + 31) // 32 * 32
@@ -57,3 +56,4 @@ class ODImResize(object):
         else:
             self.network_input_width =  ((416.0 * w / h) + 31) // 32 * 32
             self.network_input_height = 416
+    '''
