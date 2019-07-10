0d5e5969ba4f05db2d0c5865ffd9c731cf47c443
Liang-Chun Tsai <lits@microsoft.com>
2019-07-10 10:32:24 -0700

    No adjust wh

--- mtorch/azureBlobODDataset.py
+++ mtorch/azureBlobODDataset.py
@@ -56,7 +56,7 @@ class AzureBlobODDataset(torch.utils.data.Dataset):
             w, h = image.size
             for i, t in enumerate(target):
                 bbox = t["BoundingBox"]
-                iris_target.append((int(t['tagIndex']), bbox[0] * w, bbox[1] * h, (bbox[0] + bbox[2]) * w, (bbox[1] + bbox[3]) * h))
+                iris_target.append((int(t['tagIndex']), bbox[0], bbox[1], (bbox[0] + bbox[2]), (bbox[1] + bbox[3])))
             sample = self.transform(sample, dummy_target)
             return sample[0], index, h, w, iris_target
         else:
